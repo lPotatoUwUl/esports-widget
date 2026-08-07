f4341c64c45151bd678c25ada8c9cc77f471fde2
lPotatoUwUl <potatouwu69@gmail.com>
2026-08-07 00:53:11 -0400

    added scraping of data

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,64 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(unused)]
+
+
+use reqwest;
+use scraper::{Html, Selector, element_ref::Select};
+use serde::{Serialize};
+use std::{error::Error, fs::File, io::repeat};
+
+
+#[derive(Debug, Serialize)]
+struct GameCard {
+    team_names: String,
+    match_time: String,
+}
+
+#[tokio::main]
+async fn main () -> Result<(), Box<dyn Error>> {
+
+    // Fetching and parsing HTML
+    let url = "https://www.vlr.gg/matches";
+    let mut gcards: Vec<GameCard> = Vec::new();
+    let response = reqwest::get(url).await?; 
+
+    let html = response.text().await?;
+    let document = Html::parse_document(&html);
+
+    // Selecting CSS 
+
+    let name_selector = Selector::parse(".text-of").unwrap();
+    let time_selector = Selector::parse(".match-item-time").unwrap();
+    
+    // Collect Scraped Data
+
+
+     for gcard in document.select(&name_selector) {
+        let team_names = gcard
+        .select(&name_selector)
+        .next()
+        .map(|t| t.text().collect::<Vec<_>>().join(""))
+        .unwrap_or_default();
+
+        let match_time = gcard
+        .select(&time_selector)
+        .next()
+        .map(|t| t.text().collect::<Vec<_>>().join(""))
+        .unwrap_or_default();
+        
+        // match_time.to_string();
+
+        gcards.push(GameCard { team_names, match_time });
+        
+
+}
+
+    
+   
+
+
+    let x = gcards;
+    println!("{:?}", x );
+
+    Ok(())
+
 }
